7259ca84e2c305508c002f06687d2d46ea44f1ef
david-baez-bravo <david.baez3@upr.edu>
2023-09-17 16:30:09 -0400

    Implemented StreakViews to daily tasks

--- streakView.js
+++ streakView.js
@@ -1,23 +1,52 @@
-let streakData = [];
+const dailyTasksData = [
+    {
+        id: "exercise-streak", 
+        color: "#FFFF00", 
+        streakData: [] 
+    },
+    {
+        id: "abstinence-streak",
+        color: "#EE82EE",
+        streakData: []
+    },
+    {
+        id: "programming-streak",
+        color: "#39FF14",
+        streakData: []
+    },
+    {
+        id: "checking-emails-streak",
+        color: "#00FFFF",
+        streakData: []
+    },
+    {
+        id: "future-idealization-streak",
+        color: "#FFA500",
+        streakData: []
+    }
+];
 
-function addStreak() {
-    streakData.push(true);
-    renderStreakView();
-}
-
-function resetStreak() {
-    streakData = [];
-    renderStreakView();
-}
-
-function renderStreakView() {
-    const streakView = document.getElementById('streak-view');
+function renderStreakView(task) {
+    const streakView = document.getElementById(task.id);
     streakView.innerHTML = '';
-    streakData.forEach(() => {
+    task.streakData.forEach(() => {
         const streakItem = document.createElement('div');
         streakItem.classList.add('streak-item');
         streakView.appendChild(streakItem);
     });
 }
 
-renderStreakView();
+function addStreak(task) {
+    task.streakData.push(true);
+    renderStreakView(task);
+}
+
+function resetStreak(task) {
+    task.streakData = [];
+    renderStreakView(task);
+}
+
+// Initial rendering of the streak views
+dailyTasksData.forEach((task) => {
+    renderStreakView(task);
+});
